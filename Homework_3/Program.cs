e326496d02a0a009d2beb44a7d0d63e2865c4a6b
Mark Ilchenko <markilchenko18.hillel@gmail.com>
2023-09-15 09:18:35 +0300

    Додав проперті до классу User

--- Homework_3/Program.cs
+++ Homework_3/Program.cs
@@ -1,29 +1,2 @@
 ﻿using Homework_3.classes;
 
-//  Console.WriteLine("Hello, created project!");
-
-//  User user1 = new User("Mark", "Ilchenko", GenderType.Male, 2003);
-
-static void MyMethod(string fname)
-{
-    Console.Write($"Enter your {fname}: ");
-}
-
-MyMethod("userName");
-string userName = Console.ReadLine();
-
-MyMethod("lastName");
-string lastName = Console.ReadLine();
-
-MyMethod("gender");
-string genderString = Console.ReadLine();
-GenderType gender = (GenderType)Enum.Parse(typeof(GenderType), genderString, true);
-
-MyMethod("dateOfBirth");
-string dateOfBirthString = Console.ReadLine();
-int dateOfBirth = int.Parse(dateOfBirthString);
-
-
-User user1 = new User(userName, lastName, gender, dateOfBirth);
-
-Console.WriteLine(user1.ToString());
--- Homework_3/classes/User.cs
+++ Homework_3/classes/User.cs
@@ -1,57 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Homework_3.classes
 {
-    public enum GenderType
+    public enum GetMyGender
     {
         Male,
         Female
     }
 
-
     internal class User
     {
-        // Дефолтнi пропертiс.
-        public string Name { get; set; }
+        public string FirstName { get; set; }
         public string LastName { get; set; }
-        private int Age { get; }
-        public GenderType Gender { get; set; }
-
-        // Тут я вирiшив спробувати взяти вiк користувача за його датою народження.
-        public int BirthYear { get; set; }
-
-        // Тут я стукаюсь до нашиз змiнних, через Program.cs я беру вiд користувача даннi, та ставлю їх сюди.
-        public User(string name, string lastName, GenderType gender, int birthYear)
-        {
-            Name = name;
-            Gender = gender;
-            LastName = lastName;
-            BirthYear = birthYear;
-            Age = CalculateAge();
-        }
-
-        // Цей метод допомагає вирахувати нинішній вік користувача за його датою народження.
-        private int CalculateAge()
-        {
-            int currentYear = DateTime.Now.Year;
-            return currentYear - BirthYear;
-        }
-
-        // Тут я збираюсь вивести всi данні про користувача.
-        private string GetIntroductionMessage()
-        {
-            return Age >= 10
-                ? $"Hi, my name is {Name} and last name {LastName}. I am {Age} years old and {Gender}."
-                : "I am a baby.";
-        }
-
-        public override string ToString()
-        {
-            return GetIntroductionMessage();
+        public int Age { get; private set; }
+        protected GetMyGender gender { get; set; }
+        public GetMyGender Gender {
+            get => gender;
+            set => gender = value;
         }
     }
 }
