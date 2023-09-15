80a9c751c2e574428f4aa58bd7ff5b4a45e42668
Mark Ilchenko <markilchenko18.hillel@gmail.com>
2023-09-15 09:58:09 +0300

    Дати можливість користувачу ввести дані для user1

--- Homework_3/Program.cs
+++ Homework_3/Program.cs
@@ -22,4 +22,31 @@ else
     Console.WriteLine("Invalid gender input.");
 }
 
+Console.WriteLine("Please enter your year of birth:");
+int birthYear;
 
+if (int.TryParse(Console.ReadLine(), out birthYear))
+{
+    user1.BirthYear = birthYear;
+}
+else
+{
+    Console.WriteLine("The year of birth entered is invalid.");
+}
+
+Console.WriteLine("Please enter the first name:");
+string firstName = Console.ReadLine();
+
+try
+{
+    user1.FirstName = firstName;
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+Console.WriteLine("Please enter the last name:");
+user1.LastName = Console.ReadLine();
+
+Console.WriteLine(user1.ToString());
