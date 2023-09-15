6433e62da4c450774963325a8b130518add506b2
Mark Ilchenko <markilchenko18.hillel@gmail.com>
2023-09-15 10:42:09 +0300

    Почав оптимізувати код, додав функції та try/catch

--- Homework_3/Program.cs
+++ Homework_3/Program.cs
@@ -1,52 +1,92 @@
 ﻿using Homework_3.classes;
 using System;
 
-User user1 = new User();
-
-Console.WriteLine("Please enter the gender (Male/Female):");
-string genderInput = Console.ReadLine();
-
-if (Enum.TryParse(typeof(GetMyGender), genderInput, out object genderObject))
-{
-    if (genderObject is GetMyGender validGender)
-    {
-        user1.Gender = validGender;
-    }
-    else
-    {
-        Console.WriteLine("Invalid gender input.");
-    }
-}
-else
-{
-    Console.WriteLine("Invalid gender input.");
-}
-
-Console.WriteLine("Please enter your year of birth:");
-int birthYear;
-
-if (int.TryParse(Console.ReadLine(), out birthYear))
-{
-    user1.BirthYear = birthYear;
-}
-else
-{
-    Console.WriteLine("The year of birth entered is invalid.");
-}
-
-Console.WriteLine("Please enter the first name:");
-string firstName = Console.ReadLine();
 
 try
 {
-    user1.FirstName = firstName;
+    User user1 = new User();
+
+    static GetMyGender GetGenderFromInput()
+    {
+        Console.WriteLine("Please enter the gender (Male/Female):");
+        string genderInput = Console.ReadLine();
+
+        if (Enum.TryParse(typeof(GetMyGender), genderInput, out object genderObject) && genderObject is GetMyGender validGender)
+        {
+            return validGender;
+        }
+        else
+        {
+            throw new ArgumentException("Invalid gender input.");
+        }
+    }
+
+    try
+    {
+        user1.Gender = GetGenderFromInput();
+    }
+    catch (ArgumentException ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+
+    static int GetValidBirthYear()
+    {
+        Console.WriteLine("Please enter your year of birth:");
+
+        if (int.TryParse(Console.ReadLine(), out int birthYear))
+        {
+            return birthYear;
+        }
+        else
+        {
+            throw new ArgumentException("The year of birth entered is invalid.");
+        }
+    }
+
+    try
+    {
+        user1.BirthYear = GetValidBirthYear();
+    }
+    catch (ArgumentException ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+
+    static string GetValidFirstName()
+    {
+        Console.WriteLine("Please enter the first name:");
+        string firstName = Console.ReadLine();
+
+        if (string.IsNullOrWhiteSpace(firstName) || !char.IsUpper(firstName[0]) || firstName.Length < 2)
+        {
+            throw new ArgumentException("Invalid first name.");
+        }
+
+        return firstName;
+    }
+
+    try
+    {
+        user1.FirstName = GetValidFirstName();
+    }
+    catch (ArgumentException ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+
+    static string GetValidLastName()
+    {
+        Console.WriteLine("Please enter the last name:");
+        return Console.ReadLine();
+    }
+
+    user1.LastName = GetValidLastName();
+
+    Console.WriteLine(user1.ToString());
 }
-catch (ArgumentException ex)
+catch (Exception ex)
 {
-    Console.WriteLine(ex.Message);
+    Console.WriteLine($"An error occurred: {ex.Message}");
 }
 
-Console.WriteLine("Please enter the last name:");
-user1.LastName = Console.ReadLine();
-
-Console.WriteLine(user1.ToString());
