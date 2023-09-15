a7b35a561f7a66c2310b65ef5de13fdbdf30e817
Mark Ilchenko <markilchenko18.hillel@gmail.com>
2023-09-15 09:36:39 +0300

    Додав birthYear, хочу через це спробувати дiзнатись вi

--- Homework_3/classes/User.cs
+++ Homework_3/classes/User.cs
@@ -16,13 +16,51 @@ namespace Homework_3.classes
 
     internal class User
     {
-        public string FirstName { get; set; }
+
+        private string firstName;
+
+        public string FirstName {
+            get {
+                return firstName;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Значення не може бути порожнім або складатися з прогалин.");
+                }
+
+                if (!char.IsUpper(value[0])) // Перевірка на велику букву
+                {
+                    throw new ArgumentException("Ім'я має починатися з великої літери.");
+                }
+
+                if (value.Length < 2) // Перевірка на мінімальну довжину імені
+                {
+                    throw new ArgumentException("Ім'я має містити щонайменше дві літери.");
+                }
+
+                // Додаткові перевірки можуть бути додані тут
+                firstName = value;
+            }
+        }
+
         public string LastName { get; set; }
+
         public int Age { get; private set; }
+
         protected GetMyGender gender { get; set; }
+
         public GetMyGender Gender {
             get => gender;
             set => gender = value;
+        }
+
+        private int birthYear { get; set; }
+        public int BirthYear
+        {
+            get { return birthYear; }
+            set { birthYear = value; CalculateAge(); } // При встановленні року народження, перераховуємо вік
         }
 
         public User()
@@ -33,6 +71,17 @@ namespace Homework_3.classes
         public User(int age)
         {
             Age = age;
+        }
+
+        private void CalculateAge()
+        {
+            Age = DateTime.Now.Year - BirthYear;
+        }
+
+        public override string ToString()
+        {
+            string ageDescription = Age < 10 ? "I am a baby." : $"I am {Age} years old.";
+            return $"Hi, my name is {FirstName} and last name {LastName}. {ageDescription} I am {Gender}.";
         }
     }
 }
